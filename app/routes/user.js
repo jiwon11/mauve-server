32f892b3b79347ec1afdf815bc8a68217e0b9b23
sangh <jeonsangho1587@gmail.com>
2021-10-17 17:57:47 +0900

    get user data completed

--- app/routes/user.js
+++ app/routes/user.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 // custom utils And middlewares
 import * as userController from '../controllers/user.controller';
+import jwtAuth from '../middlewares/authJWT';
 
 router.post('/sign', userController.signAccount);
+router.get('/user', jwtAuth, userController.getUserData);
 
 export default router;
