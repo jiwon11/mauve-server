694d934bf06a3d97dab77d665daaba56cdca7185
Jiwon <jeewonre@naver.com>
2021-11-30 15:58:30 +0900

    verify token

--- app/libs/utils/jwt.js
+++ app/libs/utils/jwt.js
@@ -24,6 +24,7 @@ export const sign = user => {
 
 export const verify = token => {
   try {
+    console.log('verify token', token);
     const decoded = jwt.verify(token, secret);
     return {
       ok: true,
