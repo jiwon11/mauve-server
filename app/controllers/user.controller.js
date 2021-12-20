659350d2e49a5b7b6c64d7cd7f2ca19508c28edb
Jiwon <jeewonre@naver.com>
2021-12-20 17:51:51 +0900

    Feat : 회원탈퇴 기능 추가
    1. 회원 탈퇴 기능을 추가하였습니다.
    2. user collection 상에서 soft delete가 됩니다. 따라서 추후에 restore가 가능합니다.
    3. 또한 rdbms와 달리 nosql의 특성으로 user collection 상에서 삭제되어도 다른 collection에서 삭제된 document를 참조하여도 그대로 참조한 데이터를 불러 올 수 있습니다.
    4. 따라서 사용자와 코치의 attribute에 deleted 속성을 추가하여 해당 사용자와 코치가 삭제된 사용자와 코치인지를 구분합니다.
    
    Signed-off-by: Jiwon <jeewonre@naver.com>

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -61,6 +61,22 @@ export const logout = async (req, res) => {
         console.log('Cannot delete');
       }
     });
+    return res.jsonResult(200, '로그아웃 되었습니다.');
+  } catch (err) {
+    console.log(err);
+    return res.jsonResult(500, { error: 'User Controller Error', message: err.message });
+  }
+};
+
+export const withdraw = async (req, res) => {
+  try {
+    const userID = req.user.ID;
+    const { success, body } = await userService.withdraw(userID);
+    if (success) {
+      return res.jsonResult(204, body);
+    } else {
+      return res.jsonResult(body.statusCode, { message: 'User Service Error', body });
+    }
   } catch (err) {
     console.log(err);
     return res.jsonResult(500, { error: 'User Controller Error', message: err.message });
@@ -75,7 +91,7 @@ export const getUserData = async (req, res) => {
     if (userDataResult.success) {
       return res.jsonResult(200, userDataResult.body);
     } else {
-      return res.jsonResult(500, { message: 'User Service Error', body: userDataResult.body });
+      return res.jsonResult(userDataResult.body.statusCode, { message: userDataResult.body.message });
     }
   } catch (err) {
     console.log(err);
