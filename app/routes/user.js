aea7f67a871fb0cca789fd9fa97ceaee2a6bad80
Jiwon <jeewonre@naver.com>
2021-12-20 16:49:23 +0900

    Feat : 로그아웃 구현
    1. jwt를 통한 로그인의 경우 클라이언트에서 jwt를 삭제하면 되기 때문에 서버단에서 따로 처리할 로직이 없습니다.
    2. 대신 redis에 저장되어 있는 토큰을 삭제하는 로직을 구현하였습니다.
    
    Signed-off-by: Jiwon <jeewonre@naver.com>

--- app/routes/user.js
+++ app/routes/user.js
@@ -8,6 +8,7 @@ import * as userController from '../controllers/user.controller';
 import jwtAuth from '../middlewares/authJWT';
 
 router.post('/sign', upload.single('profile_img'), userController.signAccount);
+router.post('/logout', userController.logout);
 router.get('/profile/:id', jwtAuth, userController.getUserData);
 router.post('/customer_uid', jwtAuth, userController.addCustomerUid);
 router.get('/card', jwtAuth, userController.getAllCard);
