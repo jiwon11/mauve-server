2025b8f10b8b0f7da9f998e439f644532304a06d
Jiwon <jeewonre@naver.com>
2021-12-23 21:28:52 +0900

    Feat : 사용자목록 조회 기능 추가
    1. 코치가 관리하는 사용자목록을 조회하는 기능을 추가하였습니다.
    2. 사용자는 결제한 사용자만 보여줍니다.
    
    Signed-off-by: Jiwon <jeewonre@naver.com>
    (cherry picked from commit 151cde9ff869402cc6ddae069a6c9e40c43fabc7)

--- app/routes/coach.js
+++ app/routes/coach.js
@@ -10,6 +10,7 @@ import jwtAuth from '../middlewares/authJWT';
 router.post('/sign', upload.single('profile_img'), coachController.signAccount);
 router.post('/login', upload.none(), coachController.login);
 //router.get('/:id', jwtAuth, coachController.getUserData);
+router.get('/users', coachController.getUserList);
 router.get('/user/log/:userId', coachController.getUserLog);
 router.get('/user/info/:userId', coachController.getUserInfo);
 export default router;
