5e2cbcfcf86490b2e67b92087a32785099b67bd0
jiwon <jeewonre@mauve.care>
2021-12-30 13:00:33 +0900

    Feat : job queue worker 수정
    1. 알림을 받는 사용자의 역할이 user일 경우에만 worker가 실행됩니다.
    2. woker와 room 서비스 간의 모듈 연동을 추가하였습니다.

--- app/queue/notifications-queue-consumer.js
+++ app/queue/notifications-queue-consumer.js
@@ -1,4 +1,5 @@
 import NotificationService from '../services/notification.service';
+import RoomService from '../services/room.service';
 import axios from 'axios';
 
 const FLARELANE_PROJECT_ID = process.env.FLARELANE_PROJECT_ID;
@@ -8,55 +9,67 @@ export const notificationsProcess = async function (job, done) {
   console.log(`Job ${job.id} is ready!`);
   const data = job.data;
   console.log('notificationsProcess Data', data);
-  NotificationService.createByChat(data.senderId, data.senderRole, data.chatRoomId, data.chatDTO).then(createNotificationResult => {
-    if (createNotificationResult.success) {
-      const result = createNotificationResult.body;
-      axios({
-        url: `https://api.flarelane.com/v1/projects/${FLARELANE_PROJECT_ID}/notifications`,
-        method: 'post',
-        headers: { 'Content-type': 'application/json; charset=utf-8', Authorization: `Bearer ${FLARELANE_TOKEN}` },
-        data: {
-          targetTypes: 'userId',
-          targetIds: [result.body.notified_user],
-          title: result.body.title,
-          body: result.body.body
-        }
-      })
-        .then(function (response) {
-          console.log(response);
-          let responseResult;
-          if (response.statusCode == 200) {
-            responseResult = {
-              success: true,
-              body: {
-                message: '푸시 알림을 발신하였습니다.'
-              }
-            };
-          } else {
-            responseResult = {
-              success: false,
-              body: body
-            };
-          }
-          console.log(responseResult);
-          done(null);
-        })
-        .catch(function (error) {
-          if (error.response) {
-            console.log({
-              success: false,
-              body: error.response.data
-            });
-          } else if (error.request) {
-            console.log(error.request);
-          } else {
-            console.log('Error', error.message);
-          }
-          done(null);
-        });
+  const RoomRecordResult = await RoomService.simpleFindById(data.chatRoomId);
+  if (RoomRecordResult.success) {
+    const room = RoomRecordResult.body.room;
+    if (data.connectedUser.includes(room.user)) {
+      console.log('알람을 받을 사용자가 없습니다.');
+      done();
     } else {
-      console.log('success:false', createNotificationResult.body);
-      done(null);
+      NotificationService.createByChat(data.senderId, data.senderRole, data.chatDTO).then(createNotificationResult => {
+        if (createNotificationResult.success) {
+          const result = createNotificationResult.body;
+          axios({
+            url: `https://api.flarelane.com/v1/projects/${FLARELANE_PROJECT_ID}/notifications`,
+            method: 'post',
+            headers: { 'Content-type': 'application/json; charset=utf-8', Authorization: `Bearer ${FLARELANE_TOKEN}` },
+            data: {
+              targetTypes: 'userId',
+              targetIds: [result.body.notified_user],
+              title: result.body.title,
+              body: result.body.body
+            }
+          })
+            .then(function (response) {
+              console.log(response);
+              let responseResult;
+              if (response.statusCode == 200) {
+                responseResult = {
+                  success: true,
+                  body: {
+                    message: '푸시 알림을 발신하였습니다.'
+                  }
+                };
+              } else {
+                responseResult = {
+                  success: false,
+                  body: body
+                };
+              }
+              console.log(responseResult);
+              done();
+            })
+            .catch(function (error) {
+              if (error.response) {
+                console.log({
+                  success: false,
+                  body: error.response.data
+                });
+              } else if (error.request) {
+                console.log(error.request);
+              } else {
+                console.log('Error', error.message);
+              }
+              done();
+            });
+        } else {
+          console.log('success:false', createNotificationResult.body);
+          done();
+        }
+      });
     }
-  });
+  } else {
+    console.log('success:false', RoomRecordResult.body);
+    done();
+  }
 };
