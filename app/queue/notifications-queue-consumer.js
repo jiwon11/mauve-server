0dc6a107646868b448b7ce2671c44fe0f7772d20
jiwon <jeewonre@mauve.care>
2021-12-31 14:57:28 +0900

    Feat : 푸시 알림 alret 수정
    1. 푸시 알림의 형태를 수정하였습니다.
    2. 푸시 알림 내 url 파라미터를 추가하였습니다.

--- app/queue/notifications-queue-consumer.js
+++ app/queue/notifications-queue-consumer.js
@@ -20,6 +20,8 @@ export const notificationsProcess = async function (job, done) {
         if (createNotificationResult.success) {
           const result = createNotificationResult.body;
           console.log(result);
+          const dataUrl = `roomId=${room._id.toString()}&chatId=${data.chatDTO._id.toString()}&created_at=${data.chatDTO.created_at}`;
+          console.log(dataUrl);
           axios({
             url: `https://api.flarelane.com/v1/projects/${FLARELANE_PROJECT_ID}/notifications`,
             method: 'post',
@@ -27,8 +29,9 @@ export const notificationsProcess = async function (job, done) {
             data: {
               targetType: 'userId',
               targetIds: [result.notified_user.toString()],
-              title: result.title,
-              body: result.body
+              title: `MAUVE`,
+              body: `${result.title} : ${result.body}`,
+              url: dataUrl
             }
           })
             .then(function (response) {
