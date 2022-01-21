7334e8cbed94fde56d4eb17bf03b3aea869d3063
jiwon <jeewonre@mauve.care>
2022-01-21 16:08:15 +0900

    Fix : 채팅방 최근 채팅 코치 채팅만 적용 되도록 수정

--- app/services/room.service.js
+++ app/services/room.service.js
@@ -491,7 +491,7 @@ export default class roomService {
               {
                 $match: {
                   $expr: {
-                    $eq: ['$room', '$$roomId']
+                    $and: [{ $eq: ['$room', '$$roomId'] }, { $gt: ['$sender_coach', null] }]
                   }
                 }
               },
