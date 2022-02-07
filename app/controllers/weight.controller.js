ef44dee4afcbf2de614c8abf3cd615b153f910bd
jiwon <jeewonre@mauve.care>
2022-02-07 15:41:25 +0900

    Fix : 몸무게 업데이트 시 해당 채팅 업데이트
    # Conflicts:
    #	app/controllers/weight.controller.js

--- app/controllers/weight.controller.js
+++ app/controllers/weight.controller.js
@@ -57,3 +57,74 @@ export const getAll = async (req, res) => {
     return res.jsonResult(500, { message: 'Weight Controller Error', err: err.message });
   }
 };
+
+export const update = async (req, res) => {
+  try {
+    const userId = req.user.ID;
+    const weightId = req.params.id;
+    const userRole = req.user.role;
+    const kilograms = req.body.kilograms;
+    const time = req.body.time;
+    const date = moment(req.body.date).tz('Asia/seoul').format('YYYY-MM-DD');
+    //const time = moment().tz('Asia/Seoul').hour() > 12 ? 'night' : 'morning';
+    const weightDTO = { user: userId, time: time, kilograms: kilograms, date: date };
+    const weightUpdateResult = await WeightService.update(weightId, weightDTO);
+    if (weightUpdateResult.success) {
+      /*
+      const roomResult = await RoomService.getRoomIdByUserId(userId);
+      let errorMsg;
+      if (!roomResult.success) {
+        errorMsg = { message: 'Room get By User ID Service Error', err: roomResult.body };
+      }
+      const targetRoomId = roomResult.body._id.toString();
+      const io = await req.app.get('io');
+      const sockets = await io.of('/chat').in(targetRoomId).fetchSockets();
+      const connectedUser = sockets.map(socket => socket.handshake.auth._id);
+      console.log('connectedUser', connectedUser);
+      const postChatResult = await ChatService.postChat(io, connectedUser, userId, userRole, targetRoomId, weightUpdateResult.body, 'weight');
+      if (process.env.NODE_ENV === 'production') {
+        await createSlackMessage(postChatResult.body);
+      }
+      if (!postChatResult.success) {
+        errorMsg = { message: 'Chat post weight Service Error', err: postChatResult.body };
+      }
+      */
+      return res.jsonResult(200, weightUpdateResult.body);
+    } else {
+      return res.jsonResult(500, { message: 'Weight Update Service Error', err: weightUpdateResult.body });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.jsonResult(500, { message: 'Weight Controller Error', err: err.message });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const userId = req.user.ID;
+    const weightId = req.params.id;
+    const weightRemoveResult = await WeightService.remove(userId, weightId);
+    if (weightRemoveResult.success) {
+      return res.jsonResult(204, weightRemoveResult.body);
+    } else {
+      return res.jsonResult(500, { message: 'Weight Remove Service Error', err: weightRemoveResult.body });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.jsonResult(500, { message: 'Weight Controller Error', err: err.message });
+  }
+};
+
+export const updateField = async (req, res) => {
+  try {
+    const weightUpdateResult = await WeightService.updateField();
+    if (weightUpdateResult.success) {
+      return res.jsonResult(201, weightUpdateResult.body);
+    } else {
+      return res.jsonResult(500, { message: 'Weight Create Service Error', err: weightGetAllResult.body });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.jsonResult(500, { message: 'Weight Controller Error', err: err.message });
+  }
+};
