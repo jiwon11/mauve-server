a2a5f3b215e31d5445b524d0abe2d226720bc18e
jiwon <jeewonre@mauve.care>
2022-02-07 13:31:48 +0900

    Feat : 몸무게 수정 및 삭제 추가 & 몸무게 날짜 필드 추가
    # Conflicts:
    #	app/routes/weight.js
    #	app/services/weight.service.js

--- app/services/weight.service.js
+++ app/services/weight.service.js
@@ -1,4 +1,5 @@
 import WeightModel from '../models/weight';
+import ChatModel from '../models/chat';
 import mongoose from 'mongoose';
 import moment from 'moment-timezone';
 export default class WeightService {
@@ -19,7 +20,29 @@ export default class WeightService {
           }
         }
       ]);
-      return { success: true, body: weightRecord[0] };
+      console.log(weightExistRecord);
+      if (weightExistRecord.length > 0) {
+        return { success: false, body: { statusCode: 403, err: '이미 입력한 날짜와 시간대입니다.' } };
+      } else {
+        const newWeight = await WeightModel.create(weightDTO);
+        const weightRecord = await WeightModel.aggregate([
+          {
+            $match: {
+              _id: mongoose.Types.ObjectId(newWeight._id)
+            }
+          },
+          {
+            $project: {
+              kilograms: 1,
+              time: 1,
+              date: 1,
+              created_at: 1,
+              updated_at: 1
+            }
+          }
+        ]);
+        return { success: true, body: weightRecord[0] };
+      }
     } catch (err) {
       console.log(err);
       return { success: false, body: err.message };
@@ -55,6 +78,67 @@ export default class WeightService {
       }
       const weightRecord = await WeightModel.aggregate(pipeline);
       return { success: true, body: weightRecord };
+    } catch (err) {
+      console.log(err);
+      return { success: false, body: err.message };
+    }
+  }
+
+  static async update(weightId, weightDTO) {
+    try {
+      const weight = await WeightModel.findOneAndUpdate(
+        {
+          _id: weightId
+        },
+        {
+          kilograms: weightDTO.kilograms,
+          time: weightDTO.time,
+          date: weightDTO.date
+        },
+        {
+          new: true
+        }
+      );
+      if (weight) {
+        return { success: true, body: weight };
+      } else {
+        return { success: false, body: '해당 ID를 가진 몸무게 데이터가 없습니다.' };
+      }
+    } catch (err) {
+      console.log(err);
+      return { success: false, body: err.message };
+    }
+  }
+
+  static async remove(userId, weightId) {
+    try {
+      const weightExistRecord = await WeightModel.aggregate([
+        {
+          $match: {
+            _id: mongoose.Types.ObjectId(weightId),
+            user: mongoose.Types.ObjectId(userId)
+          }
+        }
+      ]);
+      if (weightExistRecord.length > 0) {
+        const weightRecord = await WeightModel.delete({ _id: mongoose.Types.ObjectId(weightId), user: mongoose.Types.ObjectId(userId) });
+        return { success: true, body: weightRecord };
+      } else {
+        return { success: false, body: '해당 유저ID와 몸무게ID를 가진 데이터가 없습니다.' };
+      }
+    } catch (err) {
+      console.log(err);
+      return { success: false, body: err.message };
+    }
+  }
+
+  static async updateField() {
+    try {
+      const updateFieldResult = await WeightModel.updateMany({}, [{ $set: { date: { $dateFromString: { dateString: { $dateToString: { format: '%Y-%m-%d', date: '$created_at' } } } } } }]);
+      const updateChatFieldResult = await ChatModel.updateMany({ tag: 'weight' }, [
+        { $set: { 'body.date': { $dateFromString: { dateString: { $dateToString: { format: '%Y-%m-%d', date: '$created_at' } } } } } }
+      ]);
+      return { success: true, body: updateFieldResult };
     } catch (err) {
       console.log(err);
       return { success: false, body: err.message };
