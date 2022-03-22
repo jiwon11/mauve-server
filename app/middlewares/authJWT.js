8f1025e338069d72834e6c61a42f011d4c219ec4
jiwon <jeewonre@mauve.care>
2022-03-22 10:17:18 +0900

    Fix : sentry setUser By id

--- app/middlewares/authJWT.js
+++ app/middlewares/authJWT.js
@@ -15,7 +15,7 @@ export default (req, res, next) => {
     if (result.ok) {
       // token이 검증되었으면 req에 값을 세팅하고, 다음 콜백함수로 갑니다.
       req.user = { ID: result.id, role: result.role };
-      Sentry.setUser({ ID: result.id, role: result.role });
+      Sentry.setUser({ id: result.id });
       next();
     } else {
       // 검증에 실패하거나 토큰이 만료되었다면 클라이언트에게 메세지를 담아서 응답합니다.
